1ed66c439d21fb016239b92f5ae7f0b9bf9711b8
mheniMerz <mheni.merzouki@nist.gov>
2022-04-25 11:27:03 -0400

    add set mac address script

---
+++ net_config/playbooks/configs/leaf20/set_mac_addresses.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 60
+
+for i in {0..48..4} 
+do
+	ip link set Ethernet"$i" address 88:88:13:AF:20:"$i"
+done;
+
---
+++ net_config/playbooks/configs/spine04/set_mac_addresses.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 60
+
+for i in {0..76..4} 
+do
+	ip link set Ethernet"$i" address 88:88:55:00:04:"$i"
+done;
+
